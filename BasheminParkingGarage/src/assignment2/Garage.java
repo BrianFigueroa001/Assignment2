27720a4452dffa499fff3a70efd2096124c2d139
Brian Figueroa <brianfigueroa001@gmail.com>
2018-09-15 19:53:56 -0400

    Added input file and created code.

--- BasheminParkingGarage/src/assignment2/Garage.java
+++ BasheminParkingGarage/src/assignment2/Garage.java
@@ -8,7 +8,7 @@ package assignment2;
 public class Garage
 { 
     private final int laneSize = 10 ;
-    private Car[] parkingLanes ;
+    private final Car[] parkingLanes ;
 
     /**
      * The constructor builds the garage with ten lanes.
@@ -20,9 +20,10 @@ public class Garage
     
     /**
      * The method takes a car and parks it in the garage.
+     * @param car Car that is arriving.
      * @return String representation of the car being parked.
      */
-    public String arrive()
+    public String arrive(Car car)
     {
         
         return null ; //Place holder
@@ -30,11 +31,11 @@ public class Garage
     
     /**
      * The method takes a car and parks it in the garage.
+     * @param car Car that is departing
      * @return String representation of the car departing.
      */
-    public String depart()
+    public String depart(Car car)
     {
         return null ;
     }
-
 }
--- BasheminParkingGarage/src/assignment2/Test.java
+++ BasheminParkingGarage/src/assignment2/Test.java
@@ -1,5 +1,39 @@
 package assignment2;
 
-public class Test {
-    
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * The test class reads from an input file to create cars and determine whether
+ * they are arriving or departing.
+ */
+public class Test
+{
+
+    public static void main() throws FileNotFoundException
+    {
+        //Reads from the input file in the root directory.
+        Scanner sc = new Scanner( new File( "garage.txt" ) );
+        //Writes to the same input file in the root directory.fdvdfgbgdfbdfdf
+        PrintWriter printWriter = new PrintWriter( "garage.txt" );
+        Garage garage = new Garage();
+        Car car;
+        String input; // Stores strings from the file. 
+
+        input = sc.next();
+        car = new Car( input );
+        input = sc.next();
+        
+        //Checks if the car is arriving or departing.
+        if ( input.equals( "ARRIVE" ) )
+        {
+            garage.arrive( car );
+        } 
+        else
+        {
+            garage.depart(car);
+        }
+    }
 }
